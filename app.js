1f2c45c8cfe84fcc3ffb410f4bb8565c35d57f19
Михаил Кукреш <kukreshmihal@yandex.ru>
2022-08-03 00:41:36 +0300

    fix: commend lint

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require("express");
+const express = require('express');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const routesCards = require("./routes/cards.js"); // импортируем роутер
-const routesUsers = require("./routes/users.js"); // импортируем роутер
+const routesCards = require('./routes/cards'); // импортируем роутер
+const routesUsers = require('./routes/users'); // импортируем роутер
 
 const app = express();
 
 // подключаемся к серверу mongo
-mongoose.connect("mongodb://localhost:27017/mestodb", {
-  useNewUrlParser: true
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
 });
 
 app.use(express.json());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "62e2b222311f2beb3b073f2b", // вставьте сюда _id созданного в предыдущем пункте пользователя 62e2b222311f2beb3b073f2b
+    _id: '62e2b222311f2beb3b073f2b', // вставьте сюда _id созданного в предыдущем пункте пользователя 62e2b222311f2beb3b073f2b
   };
 
   next();
 });
 
-app.use("/cards", routesCards); // запускаем
-app.use("/users", routesUsers); // запускаем
-app.use('*', function (req, res) {
-  res.status(404).send({ message: "Неправильный путь" });
-})
+app.use('/cards', routesCards); // запускаем
+app.use('/users', routesUsers); // запускаем
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Неправильный путь' });
+});
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  // console.log(`App listening on port ${PORT}`);
 });
--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,10 @@
 const routesCards = require('express').Router(); // создали роутер
 
-const { createCard } = require("../controllers/cards");
-const { allCards } = require("../controllers/cards");
-const { idCards } = require("../controllers/cards");
-const { likesCardPut } = require("../controllers/cards");
-const { likesCardDelete } = require("../controllers/cards");
+const { createCard } = require('../controllers/cards');
+const { allCards } = require('../controllers/cards');
+const { idCards } = require('../controllers/cards');
+const { likesCardPut } = require('../controllers/cards');
+const { likesCardDelete } = require('../controllers/cards');
 
 routesCards.post('/', createCard);
 
@@ -16,5 +16,3 @@ routesCards.put('/:id/likes', likesCardPut);
 routesCards.delete('/:id/likes', likesCardDelete);
 
 module.exports = routesCards; // экспортировали роутер
-
-
