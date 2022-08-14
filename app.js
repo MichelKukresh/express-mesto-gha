05b296276e64bcc0e59634934e41683de70830dc
Михаил Кукреш <kukreshmihal@yandex.ru>
2022-08-14 13:22:24 +0300

    fix: commend test

--- app.js
+++ app.js
@@ -42,10 +42,11 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }).unknown(true),
 }), createUser);
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
